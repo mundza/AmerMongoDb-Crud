76e0dc8069a60978a2f4cf065ee062e80259f029
Mundya <Mundya@MUNDZA>
2017-05-10 15:23:30 +0200

    Add project files.

---
+++ AmerMongoDb Crud/DbConnector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace AmerMongoDb_Crud
+{
+    public static class DbConnector
+    {
+        static IMongoClient client = new MongoClient(DbConstants.ConnectionString);
+        public static IMongoDatabase Database = client.GetDatabase(DbConstants.DatabaseName);
+    }
+}
